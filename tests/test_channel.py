b53e8d35f60b3591d1f4177b9a0138bf058c4f59
Victoria Rozhkova <rozhkova@tprs.ru>
2025-04-25 15:24:58 +0300

    add additional asserts

--- tests/test_channel.py
+++ tests/test_channel.py
@@ -6,6 +6,7 @@ from allure_commons._allure import step
 
 headers = {"Authorization": "Bearer ninp4h45ej8nzd6p4wjeo5hn7w"}
 
+
 @allure.feature('Создание чата/канала')
 @allure.title('Создание чата/канала')
 def test_create_channel(base_url):
@@ -16,11 +17,12 @@ def test_create_channel(base_url):
       2. Проверить статус код ответа = 201
       """
     channel_name = f'{uuid.uuid4()}'
-    body = {"team_id":"ktfw3dx11fradfpmsc3cekdrae","name":channel_name,"display_name":"123", "type": "O"}
+    body = {"team_id": "ktfw3dx11fradfpmsc3cekdrae", "name": channel_name, "display_name": "123", "type": "O"}
     response = create_channel(base_url, body)
     assert_status_code(response, 201)
-    is_channel_created = [x for x in get_channels(base_url).json() if x["name"] == channel_name]
-    assert is_channel_created
+    with allure.step('Проверяем что канал появился в списке каналов'):
+        assert get_channels_by_name(base_url, channel_name)
+
 
 @allure.feature('Создание чата/канала')
 @allure.title('Создание чата/канала с уже существующим именем')
@@ -33,17 +35,25 @@ def test_create_duplicate_channel(base_url):
       3. Отправить /channels запрос, передав в теле такие же данные для создания чата как в шаге 1
       4. Проверить статус код ответа = 400 и текст сообщения "A channel with that name already exists on the same team."
       """
-    body = {"team_id":"ktfw3dx11fradfpmsc3cekdrae","name":f'{uuid.uuid4()}',"display_name":"123", "type": "O"}
+    channel_name = f'{uuid.uuid4()}'
+    body = {"team_id": "ktfw3dx11fradfpmsc3cekdrae", "name": channel_name, "display_name": "123", "type": "O"}
     response = create_channel(base_url, body)
     assert_status_code(response, 201)
+    with allure.step(f'Проверяем что количество каналов с именем {channel_name} равно 1'):
+        assert len(get_channels_by_name(base_url, channel_name)) == 1
     response2 = create_channel(base_url, body)
     assert_status_code(response2, 400)
     assert_response_param(response2, "message", "A channel with that name already exists on the same team.")
+    with allure.step(f'Проверяем что количество каналов с именем {channel_name} равно 1'):
+        assert len(get_channels_by_name(base_url, channel_name)) == 1
 
-@step("Отправляем запрос на создание чата/канала")
+
 def create_channel(base_url, body):
-    return requests.post(f'{base_url}/channels', json=body, headers=headers)
+    with allure.step(f'Отправляем запрос на создание чата/канала {body["name"]}'):
+        return requests.post(f'{base_url}/channels', json=body, headers=headers)
+
 
 @step("Отправляем запрос на получение списка чатов/каналов")
-def get_channels(base_url):
-    return requests.get(f'{base_url}/channels', headers=headers)
+def get_channels_by_name(base_url, channel_name):
+    response = requests.get(f'{base_url}/channels', headers=headers)
+    return [x for x in response.json() if x["name"] == channel_name]
